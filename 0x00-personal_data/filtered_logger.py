f848a4760a608964b953d8d45e47caa05a2389b7
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-06 10:46:25 +0200

    (1)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ module to get and obfuscate logs.
 """
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -23,4 +24,23 @@ def filter_datum(fields: List[str], redaction: str,
     replace capture group 2 with xxx
     """
     regexp = '(' + '=|'.join(fields) + '=)' + f'([^{separator}]+)' + separator
-    return re.sub(regexp, r'\1xxx' + separator, message)
+    return re.sub(regexp, r'\1' + redaction + separator, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
