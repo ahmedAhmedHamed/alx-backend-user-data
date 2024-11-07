23882e00ab1ef9a342ad03f5eb4d9eae735c2d24
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-07 13:59:45 +0200

    (6)

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,8 @@ from bcrypt import hashpw
 def hash_password(password: str) -> bytes:
     """ hashes a password and returns a string of bytes """
     return hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks if a password is valid hashed password """
+    return bcrypt.checkpw(password.encode(), hashed_password)
