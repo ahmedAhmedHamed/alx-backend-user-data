a54a94e3d81ea9763cc4fd637720b1fdda0504af
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-12 00:56:57 +0200

    (3) docstrings

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,11 +6,16 @@ from flask import request
 
 
 class Auth:
+    """ class for authentication handling
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require auth """
         return False
 
     def authorization_header(self, request: request=None) -> str:
+        """ authorization header """
         return None
 
     def current_user(self, request: request=None) -> TypeVar('User'):
+        """ current user """
         return None
