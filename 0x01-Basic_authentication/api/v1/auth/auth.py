fc9c796cca35d8e8d4f29ecfcce096f39c39cc93
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-12 09:22:28 +0200

    (4)

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,8 +9,16 @@ class Auth:
     """ class for authentication handling
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require auth """
-        return False
+        """ returns True if the path is not
+        in the list of strings excluded_paths """
+        if path is None or not excluded_paths:
+            return True
+        path_cpy = path[:]
+        if path_cpy[-1] != '/':
+            path_cpy += '/'
+        if path_cpy in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request: request = None) -> str:
         """ authorization header """
