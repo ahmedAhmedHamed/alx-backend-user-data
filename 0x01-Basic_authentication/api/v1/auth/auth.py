e163db752fe882ed957dd8cb730dea500df785a2
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-12 09:24:01 +0200

    (4) authorization header implemented

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,9 @@ class Auth:
 
     def authorization_header(self, request: request = None) -> str:
         """ authorization header """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request: request = None) -> TypeVar('User'):
         """ current user """
