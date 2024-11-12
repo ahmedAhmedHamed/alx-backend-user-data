820fc4e171a5a12ba2d0b10cf7d966f626c64b5b
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-12 09:51:32 +0200

    (5) docstring

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,8 @@ if auth_type == "auth":
 
 @app.before_request
 def handle_auth():
+    """ handles authentication using auth variable.
+    """
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth is None:
