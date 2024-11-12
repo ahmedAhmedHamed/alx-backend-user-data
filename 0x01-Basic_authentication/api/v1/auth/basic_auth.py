5fdce9d90b743d12aa2ee95c2fdef7829288fa3d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-12 12:45:45 +0200

    (10)

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,8 @@
 """ Module of basic authentication class
 """
 import base64
-
+from typing import TypeVar
+from models.user import User
 from .auth import Auth
 
 
@@ -43,3 +44,20 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password."""
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        users: User = User.search({"email": user_email})
+        if users is None:
+            return None
+        if not isinstance(users, list):
+            users = [users]
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
