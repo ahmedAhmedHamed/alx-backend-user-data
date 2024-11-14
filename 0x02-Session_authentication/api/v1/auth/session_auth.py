050403a288e839ae988254b824d98c175b21abc4
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-14 16:48:02 +0200

    (2) create session func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 """ Module of session authentication class
 """
+import uuid
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """ Session authentication class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id:str = None) -> str:
+        """ creates a unique session for a user """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id
