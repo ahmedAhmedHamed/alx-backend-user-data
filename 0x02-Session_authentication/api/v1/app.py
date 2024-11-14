ff95cfd8f0867ec42c6c7fd0edf1f137cecad4dc
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-14 11:52:06 +0200

    (0) request.current_user being set and used

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,6 +36,8 @@ def handle_auth():
     if auth.current_user(request) is None:
         abort(403)
 
+    request.current_user = auth.current_user(request)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
