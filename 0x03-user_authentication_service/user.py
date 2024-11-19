2d4eaf2d1c61b3be6cbb1f6c643eb6ed8c1cf24b
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-19 18:57:36 +0200

    (0) docstring

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,11 +3,14 @@
 user model file.
 """
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
 from sqlalchemy import Column, Integer, String
+Base = declarative_base()
 
 
 class User(Base):
+    """
+    user model
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
