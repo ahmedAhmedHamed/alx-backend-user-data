34503ac175ea83c0b4e8bd9f80d874224b75b8f1
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-19 19:13:41 +0200

    (1) flush instead of commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,5 +34,5 @@ class DB:
         """ adds a new user with no checking """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
-        self._session.commit()
+        self._session.flush()
         return new_user
