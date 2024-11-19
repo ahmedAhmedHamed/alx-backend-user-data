b24af739b2c64793be5338de385b2e5b0061e45d
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-19 21:58:31 +0200

    (7)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 main app module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -12,6 +15,18 @@ app = Flask(__name__)
 def home_route():
     """ home path """
     return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ the end-point to register a user """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 if __name__ == "__main__":
