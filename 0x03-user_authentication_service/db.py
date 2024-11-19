45c204447a5e4e5bfab21a89ff89bff8cc42fc8e
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-19 20:49:38 +0200

    (2)

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,9 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -36,3 +38,17 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """
+        returns the first row found in the users table
+         as filtered by the method’s input arguments
+        """
+        column_names = [column.name for column in User.__table__.columns]
+        for key in kwargs:
+            if key not in column_names:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
