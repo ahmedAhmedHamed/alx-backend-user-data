2336bd946c092cb5d7792cb218af78d5ebc61705
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-20 15:02:04 +0200

    (15)

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -54,5 +54,15 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """ get profile path """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user or not session_id:
+        flask.abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
